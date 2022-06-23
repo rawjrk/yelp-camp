4a493ab6f8de412d21dda1867e8cd1a53c44faee
RawJrk <rodjer7777@gmail.com>
2022-06-23 15:57:37 +0300

    added basic styling

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const engine = require('ejs-mate');
 const methodOverride = require('method-override');
-
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -15,10 +15,11 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
