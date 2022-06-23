4447496c2bab1a4ae88afef30db32b659c34eac0
RawJrk <rodjer7777@gmail.com>
2022-06-23 15:57:37 +0300

    added basic styling

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
-      location: `${cities[random1000].city}, ${cities[random1000].state}`
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: 'http://source.unsplash.com/collection/484351',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem expedita, omnis perspiciatis possimus, asperiores voluptas odio fugit velit impedit enim eos quae modi minus a, ab sint. Repudiandae, magni reprehenderit.',
+      price: price
     });
     await camp.save();
   }
