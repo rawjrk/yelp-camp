2190bc9286b627e40ddff2a7caee955f06ff1479
RawJrk <rodjer7777@gmail.com>
2022-06-25 16:10:14 +0300

    set-up basic flash-messages functionality

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const path = require('path');
 const mongoose = require('mongoose');
 const engine = require('ejs-mate');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
+
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
@@ -21,9 +24,28 @@ app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+  secret: 'fsafhaha',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 3600 * 24 * 7,
+    maxAge: 1000 * 3600 * 24 * 7
+  }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next()
+})
 
 app.get('/', (req, res) => {
   res.render('home');
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -28,18 +28,27 @@ router.get('/new', (req, res) => {
 router.post('/', validateCampground, catchAsync(async (req, res) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
+  req.flash('success', 'Successfully added a new campground!');
   res.redirect(`/campgrounds/${ campground._id }`);
 }))
 
 router.get('/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id).populate('reviews');
+  if(!campground) {
+    req.flash('error', 'Cannot find that campground!');
+    return res.redirect('/campgrounds');
+  }
   res.render('campgrounds/show', { campground });
 }))
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
+  if(!campground) {
+    req.flash('error', 'Cannot find that campground!');
+    return res.redirect('/campgrounds');
+  }
   res.render('campgrounds/edit', { campground });
 }))
 
@@ -47,12 +56,14 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
   await campground.save();
+  req.flash('success', 'Successfully updated campground!');
   res.redirect(`/campgrounds/${ campground._id }`);
 }))
 
 router.delete('/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
+  req.flash('success', 'Successfully deleted campground!');
   res.redirect('/campgrounds');
 }))
 
