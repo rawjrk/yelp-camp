1cb7ad0c89550daeaf2f2a4c5949c877202cd523
RawJrk <rodjer7777@gmail.com>
2022-06-25 15:07:32 +0300

    set-up /public directory for static assets

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
