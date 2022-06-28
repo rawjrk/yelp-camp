b77ce32c0a6eca139c514daf4901249e93f7c5ce
RawJrk <rodjer7777@gmail.com>
2022-06-28 21:58:25 +0300

    integrated mapbox functionality

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -11,12 +11,25 @@ ImageSchema.virtual('thumbnail').get(function() {
   return this.url.replace('/upload', '/upload/w_200');
 });
 
+const schemaConfig = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
   title: String,
   images: [ImageSchema],
+  location: String,
+  geometry: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
   price: Number,
   description: String,
-  location: String,
   author: {
     type: Schema.Types.ObjectId,
     ref: 'User',
@@ -27,6 +40,18 @@ const CampgroundSchema = new Schema({
       ref: 'Review',
     },
   ],
+}, schemaConfig);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function() {
+  return `
+    <h4 class="mb-0">${ this.title }</h4>  
+    <span>${ this.description.substring(0, 80) }...</span>
+    <a class="text-right" href="/campgrounds/${ this._id }">Show more</a>
+    `;
+});
+
+CampgroundSchema.virtual('properties.popUpMarkupNoLink').get(function() {
+  return `<h4 class="mb-0">${ this.title }</h4><span class="text-muted">${ this.location }</span>`;
 });
 
 CampgroundSchema.post('findOneAndDelete', async campground => {
--- seeds/index.js
+++ seeds/index.js
@@ -18,13 +18,20 @@ const sample = array => {
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 200; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       author: '62b836dd48c555916faa42a5',
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       images: [
         {
           url: 'https://res.cloudinary.com/dxpthu8lk/image/upload/v1656336191/YelpCamp/kaueras6rkazbw5aekxq.jpg',
